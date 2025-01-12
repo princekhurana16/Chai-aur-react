4b7ef5f0f56002015f9f61a5d061cfa2a40a31d5
tachnicalguftgu <princekhurana16@gmail.com>
2025-01-12 13:50:42 +0530

    modifucation

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+// const anotherUser = "chai aur code "
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
